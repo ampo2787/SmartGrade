b681a8bda4ce88a2dbb6c8ee81fd5303d528f3cd
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-06-24 17:34:13 +0900

    Initial Commit

---
+++ SmartGrades/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartGrades
+//
+//  Created by JihoonPark on 24/06/2019.
+//  Copyright © 2019 JihoonPark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
