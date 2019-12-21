9893a0fbca297b4bd3e6357957a991e2d258fae9
Jihoon Park <ampo2787@naver.com>
2019-12-21 17:20:05 +0900

    simple animation

--- SmartGrades/Controller/AverageVC.swift
+++ SmartGrades/Controller/AverageVC.swift
@@ -21,8 +21,7 @@ class AverageVC: UIViewController {
     @IBOutlet weak var retakePreviousTF: UITextField!
     @IBOutlet weak var retakeAfterTF: UITextField!
     @IBOutlet weak var retakeScore: UITextField!
-    
-    @IBOutlet weak var resultScore: UILabel!
+    @IBOutlet weak var retakeOnOffImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +32,42 @@ class AverageVC: UIViewController {
     @IBAction func retakeBtnTouched(_ sender: UIButton) {
         
         if self.retakeBtn.tag == 1 {
+            self.retakeView.alpha = 0
             self.retakeView.isHidden = false
             self.retakeBtn.tag = 0
             self.retakeBtn.setTitle("재이수 안했어요.", for: .normal)
+           
+            UIView.animate(withDuration: 1, animations: {
+                if #available(iOS 13.0, *) {
+                               self.retakeOnOffImage.image = UIImage.init(systemName: "minus.circle")
+                           } else {
+                               self.retakeOnOffImage.image = UIImage.init(named: "minus.circle")
+                               // Fallback on earlier versions
+                           }
+                           
+                self.retakeView.alpha = 1
+            })
         }
         else {
-            self.retakeView.isHidden = true
             self.retakeBtn.tag = 1
             self.retakeBtn.setTitle("재이수 과목이 있다면 ?", for: .normal)
+            
+            
+            UIView.animate(withDuration: 1, animations: {
+                if #available(iOS 13.0, *) {
+                    self.retakeOnOffImage.image = UIImage.init(systemName: "keyboard.chevron.compact.down")
+                } else {
+                    self.retakeOnOffImage.image = UIImage.init(named: "keyboard.chevron.compact.down")
+                    // Fallback on earlier versions
+                }
+                                    self.retakeView.alpha = 0
+
+            }) { (complete) in
+                if complete == true {
+                    self.retakeView.isHidden = true
+                }
+            }
+            
         }
         
     }
@@ -63,8 +90,6 @@ class AverageVC: UIViewController {
             
             if self.retakeBtn.tag == 1 {
                 Calculator.MyCalculator.Calculate(lastAv: lastAverage!, allScore: allScore!, thisAv: thisAverage!, thisScore:thisScore!)
-                self.resultScore.text = String(Calculator.result)
-                
                 
             }
             else {
@@ -72,9 +97,12 @@ class AverageVC: UIViewController {
                 let retakeAfter = Float(self.retakeAfterTF.text!)
                 let retakeScore = Float(self.retakeScore.text!)
                 Calculator.MyCalculator.Calculate(lastAv: lastAverage!, allScore: allScore!, thisAv: thisAverage!, thisScore:thisScore!, retakePre: retakePrevious!, retakeAfter: retakeAfter!, retakeScore: retakeScore!)
-                self.resultScore.text = String(Calculator.result)
             }
             
+            let AlertView = UIAlertController(title: "예상 성적입니다!", message: "당신의 점수는! " + String(Calculator.result) + "입니다.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "고생했어요!", style: .cancel, handler: nil)
+            AlertView.addAction(defaultAction)
+            present(AlertView, animated: true, completion: nil)
                    
             
         }
